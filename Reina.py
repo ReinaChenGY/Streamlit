b9af473cec879271a2e83d6f9ab308b244f24c7e
Reina Chen <157318918+ReinaChenGY@users.noreply.github.com>
2024-04-22 13:40:27 -0500

    Update Reina.py

--- Reina.py
+++ Reina.py
@@ -81,12 +81,12 @@ with st.container():
             with col:
                 st.image(f'https://raw.githubusercontent.com/ReinaChenGY/Streamlit/main/photo/{project_img}', use_column_width=True, output_format='JPEG')
                 st.write(project_name)
-                # Create a button with Streamlit-like styling
-                # Create a button with adjusted height
+
+                # Create a button with specified width and height
                 button_html = f"""<form action="{project_url}" target="_blank">
                                   <input type="submit" value="Check it out!" style="color: black; background-color: white; 
-                                  border: 1px solid #cccccc; padding: 4px 16px; text-align: center; display: inline-block; font-size: 16px; 
-                                  margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+                                  border: 1px solid #cccccc; padding: 4px 16px; text-align: center; display: inline-block; 
+                                  font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px; width: 145px; height: 38px;">
                                   </form>"""
                 st.markdown(button_html, unsafe_allow_html=True)
 
